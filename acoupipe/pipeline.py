e7a631d0d4d865ebabf1b07c5766180b6f8788a0
Adam Kujawski <adam.kujawski@tu-berlin.de>
2023-01-31 10:14:28 +0100

    use dicts instead of lists

--- acoupipe/pipeline.py
+++ acoupipe/pipeline.py
@@ -17,7 +17,7 @@ from time import time
 import ray
 from numpy.random import RandomState, default_rng
 from tqdm import tqdm
-from traits.api import Callable, Either, HasPrivateTraits, Int, List, Trait, Tuple
+from traits.api import Callable, Dict, Either, HasPrivateTraits, Int, Trait, Tuple
 
 from acoupipe.sampler import BaseSampler
 
@@ -77,8 +77,7 @@ class BasePipeline(DataGenerator):
     #: a list with instances of :class:`~acoupipe.sampler.BaseSampler` derived classes
     #: alternatively, the list can contain objects of type :class:`numpy.random._generator.Generator` or
     #: :class:`numpy.random.RandomState` for control reasons
-    #: Further objects can be passed if they consist of a 'attr'`seed` attribute.
-    sampler = List([],
+    sampler = Dict(key_trait=Int, 
         desc="a list with instances of BaseSampler derived classes")
     
     #: feature method for the extraction/generation of features and labels.
@@ -94,7 +93,7 @@ class BasePipeline(DataGenerator):
     #: This seed is used to initialize an instance of :class:`numpy.random._generator.Generator` which is passed to 
     #: the :attr:`random_state` of the samplers in :attr:`sampler`. If not given, :meth:`get_data()` relies on 
     #: :attr:`numsamples`.  
-    random_seeds = List(range,
+    random_seeds = Dict(key_trait=Int, value_trait=range,
         desc="List of seeds associated with sampler objects")    
 
     #: number of samples to calculate by :meth:`get_data()`. 
@@ -105,7 +104,7 @@ class BasePipeline(DataGenerator):
     _idx = Int(0, 
         desc="Internal running index")
     
-    _seeds = List(
+    _seeds = Dict(key_trait=Int, value_trait=Int,
         desc="Internal running seeds")
 
     #: logger instance to log calculation times for each data sample
@@ -125,10 +124,10 @@ class BasePipeline(DataGenerator):
     def _validate_random_seeds(self):
         """validation of specified random seeds."""
         if self.random_seeds:
-            if len(self.random_seeds) != len(self.sampler):
+            if len(self.random_seeds.keys()) != len(self.sampler.keys()):
                 raise ValueError("Number of given range objects in random_seeds"\
                                   "and number of sampler objects need to be equal!")
-            if len(set(list(map(len,self.random_seeds)))) != 1:
+            if len(set(list(map(len,self.random_seeds.values())))) != 1:
                 raise ValueError("Length of range objects in random_seeds"\
                                  "list must be equal!")
 
@@ -144,15 +143,14 @@ class BasePipeline(DataGenerator):
         """updates seeds and running index of associated with the current data sample of the dataset."""
         self._idx += 1
         if self.random_seeds: 
-            self._seeds = list(map(next,seed_iter))
-            if self.random_seeds:
-                for i in range(len(self.sampler)):
-                    if isinstance(self.sampler[i],BaseSampler):
-                        self.sampler[i].random_state = default_rng(self._seeds[i]) 
-                    elif isinstance(self.sampler[i], RandomState):
-                        self.sampler[i].seed(self._seeds[i])
-                    else:
-                        self.sampler[i].seed = self._seeds[i]
+            self._seeds = {k : next(seed_iter[k]) for k in seed_iter.keys()}
+            for k in self.sampler.keys():
+                if isinstance(self.sampler[k],BaseSampler):
+                    self.sampler[k].random_state = default_rng(self._seeds[k]) 
+                elif isinstance(self.sampler[k], RandomState):
+                    self.sampler[k].seed(self._seeds[k])
+                else:
+                    self.sampler[k].seed = self._seeds[k]
                 
     def get_data(self, progress_bar=True):
         """provides the extracted features, sampler seeds and indices.
@@ -169,16 +167,20 @@ class BasePipeline(DataGenerator):
         """
         if self.random_seeds: 
             self._validate_random_seeds()
-            seed_iter = list(map(iter,self.random_seeds))
-            nsamples = len(self.random_seeds[0])
+            seed_iter = {k : iter(v) for k,v in self.random_seeds.items()}
+            nsamples = len(list(self.random_seeds.values())[0])
         else:
             seed_iter = None
             nsamples = self.numsamples
+        sampler_order = list(self.sampler.keys())
+        sampler_order.sort()
         self._idx = 0
         for _ in tqdm(range(nsamples),colour="#1f77b4", disable=(not progress_bar)):
             self._update_sample_index_and_seeds(seed_iter)
-            [s.sample() for s in self.sampler]
-            data = {"idx" : self._idx, "seeds": list(self._seeds)}
+            for i in sampler_order:
+                if isinstance(self.sampler[i], BaseSampler):
+                    self.sampler[i].sample()
+            data = {"idx" : self._idx, "seeds": [(k,v) for k,v in self._seeds.items()]}
             yield data.update(self._extract_features())
 
 
@@ -201,23 +203,27 @@ class SamplerActor(object):
 
     def __init__(self, sampler):
         self.sampler = sampler
+        self.sampler_order = list(self.sampler.keys())
+        self.sampler_order.sort()
 
     def sample(self, seeds):
         """Invocation of the :meth:`sample` function of one or more :class:`BaseSampler` instances."""
         self.set_new_seed(seeds)
-        [s.sample() for s in self.sampler]
+        for k in self.sampler_order:
+            if isinstance(self.sampler[k], BaseSampler):
+                self.sampler[k].sample()
         return self.sampler
 
     def set_new_seed(self, seeds=None):
-        """Re-seeds :class:`BaseSampler` instances specified in :attr:`sampler` list."""
+        """Re-seeds :class:`BaseSampler` instances specified in :attr:`sampler` dict."""
         if seeds:
-            for i in range(len(self.sampler)):
-                if isinstance(self.sampler[i],BaseSampler):
-                    self.sampler[i].random_state = RandomState(seeds[i])#default_rng(seeds[i]) 
-                elif isinstance(self.sampler[i], RandomState):
-                    self.sampler[i].seed(seeds[i])
+            for k in self.sampler.keys():
+                if isinstance(self.sampler[k],BaseSampler):
+                    self.sampler[k].random_state = default_rng(seeds[k]) 
+                elif isinstance(self.sampler[k], RandomState):
+                    self.sampler[k].seed(seeds[k])
                 else:
-                    self.sampler[i].seed = seeds[i]
+                    self.sampler[k].seed = seeds[k]
 
 class DistributedPipeline(BasePipeline):
     """Class to calculate data (extract features) in parallel to build large datasets.
@@ -242,8 +248,8 @@ class DistributedPipeline(BasePipeline):
             result_id = _extract_features.remote(sampler_ref, self.features, times) # calculation is started in new remote task 
         else:
             result_id = _extract_features.remote(sampler_ref, self.features[0], times, *list(self.features[1:]))  
-        task_dict[result_id] = {"idx" : self._idx, "seeds": list(self._seeds)}  # add sample index, and seeds)
-
+        task_dict[result_id] = {"idx" : self._idx, "seeds": [(k,v) for k,v in self._seeds.items()]}  # add index, and seeds
+        
     def _log_execution_time(self,task_index,times,pid):
         self.logger.info("id %i on pid %i: scheduling task took: %2.32f sec" % \
         (task_index, pid, times[1]-times[0]))
@@ -256,8 +262,7 @@ class DistributedPipeline(BasePipeline):
         # self.logger.info("%i args:[%r] took: %2.4f sec" % \
         # (f.__name__,args,end-start))
 
-    def _sample_and_schedule_task(self,sampler_actor,task_dict,seed_iter):
-        self._update_sample_index_and_seeds(seed_iter)
+    def _sample_and_schedule_task(self,sampler_actor,task_dict):
         sampler_ref = sampler_actor.sample.remote(self._seeds)
         self.logger.info("id %i: start task." %self._idx)
         self._schedule(sampler_ref,task_dict)
@@ -266,7 +271,7 @@ class DistributedPipeline(BasePipeline):
         """updates seeds and running index of associated with the current data sample of the dataset."""
         self._idx += 1
         if self.random_seeds: 
-            self._seeds = list(map(next,seed_iter))
+            self._seeds = {k : next(seed_iter[k]) for k in seed_iter.keys()}
 
     def get_data(self, progress_bar=True):
         """Provides the extracted features, sampler seeds and indices.
@@ -290,8 +295,8 @@ class DistributedPipeline(BasePipeline):
         sampler_actors = [SamplerActor.remote(sampler=self.sampler) for _ in range(self.numworkers)]
         if self.random_seeds: 
             self._validate_random_seeds()
-            seed_iter = list(map(iter,self.random_seeds))
-            nsamples = len(self.random_seeds[0])
+            seed_iter = {k : iter(v) for k,v in self.random_seeds.items()}
+            nsamples = len(list(self.random_seeds.values())[0])
         else:
             seed_iter = None
             nsamples = self.numsamples
@@ -300,7 +305,8 @@ class DistributedPipeline(BasePipeline):
         finished_tasks = 0
         actor_idx = 0
         for _ in range(min(nsamples,self.numworkers)): 
-            self._sample_and_schedule_task(sampler_actors[actor_idx],task_dict,seed_iter)
+            self._update_sample_index_and_seeds(seed_iter)
+            self._sample_and_schedule_task(sampler_actors[actor_idx],task_dict)
             actor_idx = (actor_idx + 1) % self.numworkers
         while finished_tasks < nsamples: 
             done_ids, pending_ids = ray.wait(list(task_dict.keys()))
@@ -318,7 +324,8 @@ class DistributedPipeline(BasePipeline):
                 self.logger.info("id %i on pid %i: finished task." %(data["idx"],pid))
                 self._log_execution_time(data["idx"], times, pid)
                 if (nsamples - self._idx) > 0: # directly _schedule next task
-                    self._sample_and_schedule_task(sampler_actors[0],task_dict,seed_iter)
+                    self._update_sample_index_and_seeds(seed_iter)
+                    self._sample_and_schedule_task(sampler_actors[0],task_dict)
                     actor_idx = (actor_idx + 1) % self.numworkers
                 progress_bar.update()
                 yield data
