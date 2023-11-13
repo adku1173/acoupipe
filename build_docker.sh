710cbc6c0af2120d0eb4ebbfe24efcd6ebabc0ad
Adam Kujawski <adam.kujawski@tu-berlin.de>
2023-11-13 17:59:07 +0100

    rechange

--- build_docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#docker build . --file Dockerfile --target base --build-arg TOKEN="xfkM4ZhWP-LSRMWg2JWu" --tag adku1173/acoupipe:dev
-
-#DOCKER_BUILDKIT=1 docker build . --file Dockerfile --target dev --build-arg TOKEN="xfkM4ZhWP-LSRMWg2JWu" --tag adku1173/acoupipe:dev-dev
-DOCKER_BUILDKIT=1 docker build . --file Dockerfile --target base --build-arg TOKEN="xfkM4ZhWP-LSRMWg2JWu" --tag adku1173/acoupipe:dev-base
