7f60e15972f203527034c672be3ec0dc47934f08
Adam Kujawski <kujawski.ad@gmail.com>
2025-11-11 13:10:21 +0100

    wip

--- docs/source/contents/script/reverb.py
+++ docs/source/contents/script/reverb.py
@@ -1,5 +1,4 @@
-
-#%%
+# %%
 
 
 from pathlib import Path
@@ -10,16 +9,16 @@ from modelsdfg.transformer.datasets import ConfigUMAReverb
 
 from acoupipe.datasets.reverb import DatasetISM
 
-#%%
+# %%
 
 
 mics = ac.MicGeom(from_file=Path(ac.__file__).parent / "xml" / "minidsp_uma-16_mirrored.xml")
 
 config = ConfigUMAReverb(
-        mode="wishart",
-        random_signal_length = True,
-        mic_pos_noise=False,
-        )
+    mode="wishart",
+    random_signal_length=True,
+    mic_pos_noise=False,
+)
 
 dataset = DatasetISM(
     # mode = "analytic",
@@ -29,13 +28,19 @@ dataset = DatasetISM(
     # mic_pos_noise = False,
     # signal_length = 10,
     config=config,
-    tasks=1, num_gpus=1,
-    )
+    tasks=1,
+    num_gpus=1,
+)
 # generate data for frequency 2000 Hz (single frequency)
 data_generator = dataset.generate(
     features=["sourcemap", "csm", "source_strength_analytic", "source_strength_estimated", "loc", "f"],
-    #features=["csm"],
-                                    split="training", size=1000, f=[4000], num=0, start_idx=1)
+    # features=["csm"],
+    split="training",
+    size=1000,
+    f=[4000],
+    num=0,
+    start_idx=1,
+)
 data_sample = next(data_generator)
 
 # for data_sample in enumerate(data_generator):
@@ -46,12 +51,11 @@ if data_sample.get("source_strength_analytic") is not None:
 if data_sample.get("source_strength_estimated") is not None:
     print(ac.L_p(data_sample["source_strength_estimated"]))
 if data_sample.get("csm") is not None:
-    print(ac.L_p(data_sample["csm"][0,0,0]))
+    print(ac.L_p(data_sample["csm"][0, 0, 0]))
 if data_sample.get("sourcemap") is not None:
     print(ac.L_p(data_sample["sourcemap"]).max())
 
 if data_sample.get("sourcemap") is not None and data_sample.get("loc") is not None and data_sample.get("f") is not None:
-
     extent = dataset.config.msm_setup.grid.extend()
 
     # sound pressure level
@@ -61,7 +65,7 @@ if data_sample.get("sourcemap") is not None and data_sample.get("loc") is not No
 
     # plot sourcemap
     fig = plt.figure()
-    plt.title(f'Beamforming Map (f={data_sample["f"][0]} Hz)')
+    plt.title(f"Beamforming Map (f={data_sample['f'][0]} Hz)")
     plt.imshow(Lm, vmax=Lm_max, vmin=Lm_min, extent=extent, origin="lower")
     plt.colorbar(label="Sound Pressure Level (dB)")
 
@@ -72,17 +76,16 @@ if data_sample.get("sourcemap") is not None and data_sample.get("loc") is not No
     plt.ylabel("y (m)")
 
 
+# import numpy as np
 
-import numpy as np
+# plt.figure()
+# freqs = config.msm_setup.freq_data.transfer.fftfreq()
+# H_j0 = config.msm_setup.freq_data.transfer._get_r0_transfer().squeeze()
+# H_rm_0 = config.msm_setup.freq_data.transfer._get_rm_transfer()[:,0].squeeze()
 
-plt.figure()
-freqs = config.msm_setup.freq_data.transfer.fftfreq()
-H_j0 = config.msm_setup.freq_data.transfer._get_r0_transfer().squeeze()
-H_rm_0 = config.msm_setup.freq_data.transfer._get_rm_transfer()[:,0].squeeze()
-
-plt.plot(freqs,10*np.log10(np.abs(H_j0)))
-plt.plot(freqs,10*np.log10(np.abs(H_rm_0)))
-plt.show()
+# plt.plot(freqs,10*np.log10(np.abs(H_j0)))
+# plt.plot(freqs,10*np.log10(np.abs(H_rm_0)))
+# plt.show()
 
 
 # #%%
@@ -135,7 +138,6 @@ plt.show()
 #     signal = pyfar.Signal(kernels.T[:2], sampling_rate=dataset.config.fs)
 #     pyfar.plot.freq(signal)
 #     #pyfar.plot.phase(signal)
-
 
 
 # #%%
@@ -209,14 +211,11 @@ plt.show()
 #     break
 
 
-
-
 # #%%
 # from pathlib import Path
 
 # dpath = Path(__file__).parent.parent.parent / "_static"
 # fig.savefig(dpath / "ISM_sourcemap.png", dpi=300)
-
 
 
 # # %%
--- examples/rir_example.py
+++ examples/rir_example.py
@@ -1,41 +1,121 @@
+from pathlib import Path
+
 import acoular as ac
 import matplotlib.pyplot as plt
 import numpy as np
 
-from acoupipe.config import HAVE_GPURIR
+from acoupipe.datasets.spectra_analytic import PowerSpectraAnalytic
+from acoupipe.datasets.transfer import TransferGpuRIR
 from acoupipe.datasets.utils import get_absorption_coeff
 
 rng = np.random.RandomState(1)
 rng2 = np.random.RandomState(1)
 
-alpha = get_absorption_coeff(rng, realistic_walls=True)
+
+block_size = 128
+fs = 13720
+signal_length = 10  # seconds
 alpha_off = get_absorption_coeff(rng2, realistic_walls=False)
+# alpha_off = get_absorption_coeff(rng2, realistic_walls=True)
+# alpha_off = np.ones_like(alpha_off)
 
-print(alpha)
-print(alpha_off)
+# source locations: array of (3,num_sources):
+pos = np.array([[-0.5602875, -0.43781025], [0.92770928, -0.03813699], [1.5, 1.5]])
+num_sources = pos.shape[1]
 
-if HAVE_GPURIR:
-    from acoupipe.datasets.transfer import TransferGpuRIR
+# source strengths (all equal)
+Q = np.zeros((block_size // 2 + 1, num_sources, num_sources), dtype=np.complex128)
+for i in range(num_sources):
+    Q[:, i, i] = 1
 
-    mics = ac.MicGeom(mpos_tot=np.abs(np.random.normal(size=64*3)).reshape(3, -1))
-    grid = ac.ImportGrid(gpos_file=np.abs(np.random.normal(size=6)).reshape(3, -1))
-    trans = TransferGpuRIR(sample_freq=51200, block_size=1024,
-        mics=mics, grid=grid, room_size=[6, 6, 6], alpha=alpha_off.mean(1), rel_tdir=np.array([0.5, 0.5, 0.5]))
-    rir = trans.rir
-    transfer = trans.transfer()
-    mag_transfer = 20*np.log10(np.abs(transfer))
+# ref position
+ref = np.array([-0.021, -0.063, 0.0])
 
-    plt.figure()
-    plt.title("RIR")
-    plt.plot(rir[0, 0, :])
-
-    plt.figure()
-    plt.title("Transfer")
-    plt.plot(trans.fftfreq(), mag_transfer[0, 0, :])
-    plt.xscale("log")
+# room parameters
+room_size = np.array([2.18040357, 2.85165386, 2.17179724])
+alpha = np.array([0.14632144, 0.32326808, 0.67843576, 0.81814866, 0.49110407, 0.86437483])  # absorption coefficients per wall
+origin = np.array([1.08414871, 1.40887475, 0.18653786])  # origin of the room
 
 
+mics = ac.MicGeom(file=Path(ac.__file__).parent / "xml" / "minidsp_uma-16_mirrored.xml")
+source_grid = ac.ImportGrid(pos=pos)
+
+# define the transfer function (via ISM model)
+trans = TransferGpuRIR(
+    ref=ref,
+    sample_freq=fs,
+    block_size=block_size,
+    mics=mics,
+    grid=source_grid,
+    room_size=room_size,
+    alpha=alpha_off[:, 0],
+    origin=origin,
+)
+
+csm = PowerSpectraAnalytic(
+    Q=Q,
+    transfer=trans,
+    mode="wishart",
+    block_size=block_size,
+    overlap="50%",
+    numsamples=signal_length * fs,
+    sample_freq=fs,
+)
 
 
+# get the CSM for the Transformer:
+csm_full = csm.csm
+print(csm_full.shape)
+
+# Beamforming
+rg = ac.RectGrid(
+    x_min=-1.5,
+    x_max=1.5,
+    y_min=-1.5,
+    y_max=1.5,
+    z=1.5,
+    increment=0.05,
+)
+
+steer = ac.SteeringVector(
+    mics=mics,
+    grid=rg,
+    ref=trans.ref,
+)
+
+bb = ac.BeamformerBase(
+    freq_data=csm,
+    steer=steer,
+)
+
+Lm = ac.L_p(bb.synthetic(4000, 3)).T
+
+# source map
+fig = plt.figure()
+plt.title("Beamforming Map")
+extent = rg.extend()
+plt.imshow(Lm, extent=extent, origin="lower", vmax=Lm.max(), vmin=Lm.max() - 20)
+plt.colorbar(label="Sound Pressure Level (dB)")
+# annotate source locations
+for loc in pos.T:
+    plt.scatter(loc[0], loc[1])
+
+# plot transfer function
+
+plt.figure()
+plt.title("Transfer Function Power Spectra")
 
 
+# plot ref channel
+for i in range(0, num_sources):
+    tf = trans.transfer()[i]
+    tfps = np.real(tf * tf.conj())
+
+    plt.plot(trans.fftfreq(), 10 * np.log10(tfps[0]), label="ref")
+
+    for m in range(1, 15):
+        plt.plot(trans.fftfreq(), 10 * np.log10(tfps[m]), label=f"mic {m + 1}")
+plt.legend()
+plt.ylim(-60, 60)
+plt.xscale("log")
+plt.xlim(500, 8000)
