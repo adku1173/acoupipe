c82aff0facefe53cb4dc2ecafca90bb9e2831f46
Adam Kujawski <kujawski.ad@gmail.com>
2026-04-10 15:12:32 +0200

    fixifix

--- src/acoupipe/datasets/micgeom.py
+++ src/acoupipe/datasets/micgeom.py
@@ -1,7 +1,6 @@
 from pathlib import Path
+import acoular as ac
 
-from acoular import MicGeom, __file__
-
-mics = MicGeom(file=Path(__file__).parent / 'xml' / 'tub_vogel64.xml')
+mics = ac.MicGeom(file=Path(ac.__file__).parent / 'xml' / 'tub_vogel64.xml')
 tub_vogel64 = mics.pos.copy()
 tub_vogel64_ap1 = tub_vogel64 / mics.aperture
