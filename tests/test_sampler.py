30ca42cb2733532be3e4ab0328ce2aa92739fc7b
jtodev <jan@ostendorf.com>
2026-07-06 15:47:49 +0200

    Fix pipeline_write_h5 example linting

--- tests/test_sampler.py
+++ tests/test_sampler.py
@@ -247,5 +247,5 @@ def test_valid_pipeline_funcs(base_pipeline, finput):
 def test_invalid_pipeline_funcs(base_pipeline, finput):
     _, _ = base_pipeline
     pipeline = BasePipeline(numsamples=2, features=finput)
-    with pytest.raises(ValueError):
+    with pytest.raises(TypeError):
         next(pipeline.get_data(progress_bar=False))
