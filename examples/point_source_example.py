b9a764993a6a2ae20da2908a82818b21e76996c0
jtodev <jan@ostendorf.com>
2026-07-06 15:25:58 +0200

    (PLR2044) Remove empty comments in example RNG setup

--- examples/point_source_example.py
+++ examples/point_source_example.py
@@ -10,8 +10,8 @@ from pylab import colorbar, figure, imshow, plot, show
 ac.config.global_caching = 'none'
 
 rng1 = np.random.RandomState(1)  # scipy listens to numpy random seed (when scipy seed is None)
-rng2 = np.random.RandomState(2)  #
-rng3 = np.random.RandomState(3)  #
+rng2 = np.random.RandomState(2)
+rng3 = np.random.RandomState(3)
 
 z = 0.5  # distance between array and source plane
 
